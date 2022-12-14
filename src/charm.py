68a51b397e552461771035a85c86bd50d46ae494
Jaime Freire de Souza <jaimefreire.souza@gmail.com>
2022-12-14 10:23:56 -0300

    ASP-2378: Add an action to show the version of license-manager-cli (#9)
    Display all information returned by "pip show license-manager-cli",
    which includes the version of the package.
    
    Add an event handler for the upgrade-charm event. This is needed to 
    handle the upgrade-charm event, which updates the charm version 
    when upgraded.
    
    Update changelog

--- src/charm.py
+++ src/charm.py
@@ -31,9 +31,11 @@ class LicenseManagerCliCharm(CharmBase):
 
         event_handler_bindings = {
             self.on.install: self._on_install,
+            self.on.upgrade_charm: self._on_upgrade,
             self.on.config_changed: self._on_config_changed,
             self.on.remove: self._on_remove,
             self.on.upgrade_action: self._on_upgrade_action,
+            self.on.show_version_action: self._on_show_version_action,
         }
         for event, handler in event_handler_bindings.items():
             self.framework.observe(event, handler)
@@ -59,6 +61,11 @@ class LicenseManagerCliCharm(CharmBase):
     def _on_upgrade(self, event):
         """Perform upgrade operations."""
         self.unit.set_workload_version(Path("version").read_text().strip())
+
+    def _on_show_version_action(self, event):
+        """Show the info and version of license-manager-cli."""
+        info = self._license_manager_cli_ops.get_version_info()
+        event.set_results({"license-manager-cli": info})
 
     def _on_config_changed(self, event):
         """Configure license-manager-cli with charm config."""
